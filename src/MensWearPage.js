fea30dd196ae12802fe2e443df3f65b92a1e47ee
Xtrmcoder <mailme.pawaghmare@gmail.com>
2024-11-09 15:28:45 +0530

    vscode

---
+++ src/MensWearPage.js
@@ -0,0 +1,14 @@
+// src/pages/MensWearPage.js
+import React from "react";
+
+const MensWearPage = () => {
+  return (
+    <div>
+      <h1>Mens Wear Collection</h1>
+      <p>This page contains our Mens Wear collection.</p>
+      {/* Add more content related to Mens Wear */}
+    </div>
+  );
+};
+
+export default MensWearPage;
